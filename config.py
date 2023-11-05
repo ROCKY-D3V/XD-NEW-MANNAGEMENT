df9d58bd7c5f76b0f5bbb5c25216723fa688d933
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-11-05 09:38:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     OWNER_ID = int(getenv("OWNER_ID", "6545714937"))  # sᴛᴀʀᴛ @ADVANCE_XD_BOT ᴛʏᴘᴇ /id
     OWNER_USERNAME = getenv("OWNER_USERNAME", "ROCKY_ISS_BACK")
     SUPPORT_CHAT = getenv("SUPPORT_CHAT", "XD_N3TWORK")
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001885729197)
+    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001885729197")
     MONGO_URI = getenv("MONGO_DB_URI")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
     DATABASE_URL = getenv("DATABASE_URL")
